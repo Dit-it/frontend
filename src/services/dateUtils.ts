0e503b3851127d5bf977eff57175d0c3f068b6d6
jhlee <jhlee9462@github.com>
2024-10-06 09:43:35 +0900

    fix. 조사 일시 표시 형식 수정

--- src/services/dateUtils.ts
+++ src/services/dateUtils.ts
@@ -4,5 +4,5 @@ export const getDateString = (date : Date) => {
         date?.getMonth() + 1 > 9 ? date?.getMonth() + 1 : '0' + (date?.getMonth() + 1)}${
         // @ts-ignore
         date?.getDate() > 9 ? date?.getDate() : '0' + date?.getDate()}${
-        date?.getHours()}${date?.getMinutes()}`;
+        date?.getHours() > 9 ? date?.getHours() : '0' + date?.getHours()}${date?.getMinutes() > 9 ? date?.getMinutes() : '0' + date?.getMinutes()}`;
 }
